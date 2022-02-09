3714abce684f42f3fa019790d6ce0c27586bb6d2
nchudinov <nchudinov@dataarmor.ru>
2022-02-09 16:12:26 +0300

    Initial commit

---
+++ src/test/java/com/nchudinov/keygen/KeyGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nchudinov.keygen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
