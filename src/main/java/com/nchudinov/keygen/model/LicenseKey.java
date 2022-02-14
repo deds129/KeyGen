8b6ead91908e7897febad773e90253d3fb48aa24
deds129 <nik.chudinov129@mail.ru>
2022-02-14 16:16:38 +0300

    remove lombok

--- src/main/java/com/nchudinov/keygen/model/LicenseKey.java
+++ src/main/java/com/nchudinov/keygen/model/LicenseKey.java
@@ -1,14 +1,9 @@
 package com.nchudinov.keygen.model;
 
-import lombok.*;
-
 import javax.persistence.*;
 
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "license_keys")
 public class LicenseKey {
 	
@@ -36,6 +31,19 @@ public class LicenseKey {
 	@Column(name = "key", nullable = false)
 	private String key;
 
+	public LicenseKey() {
+	}
+
+	public LicenseKey(Integer id, String customer, String licenseType, String host, Integer port, String operSystem, String key) {
+		this.id = id;
+		this.customer = customer;
+		this.licenseType = licenseType;
+		this.host = host;
+		this.port = port;
+		this.operSystem = operSystem;
+		this.key = key;
+	}
+
 	public LicenseKey(String customer, String licenseType, String host, Integer port, String operSystem, String key) {
 		this.customer = customer;
 		this.licenseType = licenseType;
@@ -43,5 +51,74 @@ public class LicenseKey {
 		this.port = port;
 		this.operSystem = operSystem;
 		this.key = key;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(String customer) {
+		this.customer = customer;
+	}
+
+	public String getLicenseType() {
+		return licenseType;
+	}
+
+	public void setLicenseType(String licenseType) {
+		this.licenseType = licenseType;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public Integer getPort() {
+		return port;
+	}
+
+	public void setPort(Integer port) {
+		this.port = port;
+	}
+
+	public String getOperSystem() {
+		return operSystem;
+	}
+
+	public void setOperSystem(String operSystem) {
+		this.operSystem = operSystem;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	@Override
+	public String toString() {
+		return "LicenseKey{" +
+				"id=" + id +
+				", customer='" + customer + '\'' +
+				", licenseType='" + licenseType + '\'' +
+				", host='" + host + '\'' +
+				", port=" + port +
+				", operSystem='" + operSystem + '\'' +
+				", key='" + key + '\'' +
+				'}';
 	}
 }
